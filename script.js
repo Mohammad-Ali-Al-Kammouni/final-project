8156db4e4023cbb0b3b2ace1eeb9961f1d117e06
Mohammad-Ali-Al-Kammouni <mhmdalikamune@gmail.com>
2025-11-28 11:20:23 +0200

    initials

---
+++ script.js
@@ -0,0 +1,13 @@
+const bookmarkForm = document.getElementById("bookmarkForm");
+const titleInput = document.getElementById("titleInput");
+const urlInput = document.getElementById("urlInput");
+const errorMessage = document.getElementById("errorMessage");
+
+const bookmarksContainer = document.getElementById("bookmarksContainer");
+const emptyState = document.getElementById("emptyState");
+
+const themeToggleBtn = document.getElementById("themeToggleBtn");
+const cardsViewBtn = document.getElementById("cardsViewBtn");
+const listViewBtn = document.getElementById("listViewBtn");
+
+let currentTheme = "light";
